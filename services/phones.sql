2ba25a7df45132a324372bb90179ebfc82ca8e48
Allen Shi <allenshi@ncut.edu.tw>
2016-08-12 23:21:56 +0800

    sql file

--- services/phones.sql
+++ services/phones.sql
@@ -24,11 +24,6 @@ PRIMARY KEY ( `pid` )
 -- email email
 -- 職稱 title 
 -- 備註 mynote 
-
- CREATE TABLE `ncut`.`phones` ( `pid` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR( 32 ) DEFAULT NULL, `deptname` VARCHAR( 128 ) DEFAULT NULL, `phone` VARCHAR( 12 ) NOT NULL , `title` VARCHAR( 128 ) NOT NULL , `mynote` VARCHAR( 256 ) DEFAULT NULL, PRIMARY KEY ( `pid` ) ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT = 'phones'
-
-
-
 --
 -- Dumping data for table `customers`
 --
@@ -37,6 +32,6 @@ INSERT INTO `ncut`.`phones` (`pid`, `phone` ,`name`, `deptname`, `title`, `mynot
 (NULL,"2101",	"賴副校長"  , "校長室" ,"副校長",""),
 (NULL,"2110",	"李副校長"  , "校長室" ,"副校長",""),
 (NULL,"2111",	"陳主任秘書"  , "秘書室" ,"主任秘書",""),
-(NULL,"2112",	"吳專員"  , "秘書室" ,"專員",""),
+(NULL,"2112",	"吳專員"  , "秘書室" ,"專員","")
 
 
